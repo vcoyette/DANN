6dd17915d4f6713adccdcc76be4d70ff8e6d456d
vcoyette <vincent.coyette@gmail.com>
2020-04-24 11:48:20 +0200

    feature: add test script

---
+++ test.py
@@ -0,0 +1,24 @@
+"""Functions to test."""
+import torch
+
+
+def test(network, testloader):
+    """Test the network on the test data loaer.
+
+    Keyword Arguments:
+        network -- the neural network to test
+        device -- the device on which the network is
+        testloader -- the dataloader for the dataset
+    """
+    device = next(network.parameters()).device
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = network(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return correct, total
